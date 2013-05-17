1c2a9c745cd458347a0e0fec2b777c23ffd4aba9
lkitching <lee.kitching@gmail.com>
2013-05-17 14:25:24 +0100

    Initial commit. Add F# version of minesweeper kata.
    The project consists of two modules - one for representing and manipulating
    2-dimensional boards, and the other is the main module which contains the
    functions for reading, mapping and displaying board types. These could be
    placed in their own modules.

---
+++ fsharp/Minesweeper/Board.fs
@@ -0,0 +1,45 @@
+﻿module Board
+
+type Board<'t> = 't [,]     //board is a 2d array
+[<Measure>] type r          //row index type
+[<Measure>] type c          //column index type
+[<Measure>] type w          //board with type
+[<Measure>] type h          //board height type
+
+//initialises a board with the given dimensions
+//The function f is given the position on the board and returns the value at that position
+let init<'T> (width: int<w>) (height: int<h>) f : Board<'T> =
+    Array2D.init (int height) (int width) (fun row col -> f (row * 1<r>) (col * 1<c>))
+
+//gets the width of the board
+let getWidth<'T> (board: Board<'T>) = Array2D.length2 board
+
+//gets the height of the board
+let getHeight<'T> (board: Board<'T>) = Array2D.length1 board
+
+//get the neighbouring squares for a given board position
+let getNeighbours<'T> (board: Board<'T>) (row: int<r>) (col: int<c>) =
+    let height = getHeight board |> int
+    let width = getWidth board |> int
+    seq {
+        for ro in [-1; 0; 1] do
+            for co in [-1; 0; 1] do
+                let (r, c) = ((int row) + ro, (int col) + co)
+                if r >= 0 && r < height && c >= 0 && c < width then yield (r * 1<r>, c * 1<c>)
+    }
+
+//get the value at the given position
+let get<'T> (board: Board<'T>) (row: int<r>) (col: int<c>) = Array2D.get board (int row) (int col)
+
+//maps this board to another with the same dimensions
+let mapi<'T, 'U> (f: int<r> -> int<c> -> 'T -> 'U) (board: Board<'T>) : Board<'U> = Array2D.mapi (fun row col e -> f (row * 1<r>) (col * 1<c>) e) board
+
+//gets the row with the given row index
+let getRow<'T> (board: Board<'T>) (row: int<r>) = 
+    let widthi = getWidth board |> int
+    Array.init widthi (fun i -> get board row (i * 1<c>))
+
+//maps the rows on the board with a given mapping function
+let mapRows<'T, 'U> (f: 'T array -> 'U) (board: Board<'T>) : 'U seq =
+    let rowCount = getHeight board
+    Seq.init rowCount (fun r -> getRow board (r * 1<r>) |> f)
---
+++ fsharp/Minesweeper/Program.fs
@@ -0,0 +1,86 @@
+﻿open System.IO
+open Board
+
+type Square = Bomb | Clear              //inpur board square type
+type AdjSquare = Bomb | Adj of int      //output board square type with number of adjacent bombs
+
+//converts an input square to an output square given the square and a list of its neighbours
+let toAdj neighbours = function
+| Square.Bomb -> AdjSquare.Bomb
+| Square.Clear -> let adjBombs = neighbours |> Seq.filter ((=)Square.Bomb) |> Seq.length in AdjSquare.Adj(adjBombs)
+
+//maps an input board to an output board
+let mapAdjacancies (board: Square Board) =
+    let mapSquare ri ci square =
+        let neighbours = Board.getNeighbours board ri ci |> Seq.map (fun (r, c) -> Board.get board r c)
+        toAdj neighbours square
+    Board.mapi mapSquare board
+
+//parsing
+let parseSquare = function
+| '*' -> Square.Bomb
+| '.' -> Square.Clear
+| c -> failwith (sprintf "Unexpected board character %c" c)
+
+//parses a board header line. This should a line containing two ints (height width) separated by a space
+let parseHeader (header: string) =
+    let [| heightStr; widthStr |] = header.Split(' ')
+    let height = int heightStr
+    let width = int widthStr
+    (width * 1<w>, height * 1<h>)
+
+//parses a board of given dimensions contained in an array of strings whose first line is found at the given index
+let parseBoard width height (lines: string array) startIndex =
+    let getSquare (ri: int<r>) (ci: int<c>) =
+        lines.[startIndex + (int ri)].[int ci] |> parseSquare
+    Board.init width height getSquare
+
+//reads all the input boards from an array of input lines
+//the first line should be the header line for the first board
+let readAll (lines: string array) =
+    let readBoardAt (idx, num) =
+        let header = lines.[idx]
+        match parseHeader header with
+        | (0<w>, 0<h>) -> None
+        | (w, h) ->
+            let board = parseBoard w h lines (idx + 1)
+            let nextIndex = idx + (int h) + 1
+            Some((num, board), (nextIndex, num + 1))
+    Seq.unfold readBoardAt (0, 1)
+
+//formats a single output board square as a char
+let displaySquare = function
+| AdjSquare.Bomb -> '*'
+| AdjSquare.Adj(i) -> i.ToString().[0]
+
+//formats a single output board row as a string
+let displayRow row = Array.map displaySquare row |> (fun chars -> new System.String(chars))
+
+//formats an entire output board as a string, with a header row and each row on its own line
+let displayField (num: int) board =
+    let sb = new System.Text.StringBuilder()
+    sb.AppendLine(sprintf "Field #%d" num) |> ignore
+    let boardLines = Board.mapRows displayRow board |> Seq.iter(fun line -> sb.AppendLine(line) |> ignore)
+    sb.ToString()
+
+//writes a list of formatted field strings to a text writer, separated by empty lines
+let rec writeFieldsTo (tr: System.IO.TextWriter) = function
+| [] -> ()
+| [last: string] -> tr.Write(last)
+| (str::strs) ->
+    tr.Write(str)
+    tr.WriteLine()
+    writeFieldsTo tr strs
+
+[<EntryPoint>]
+let main argv = 
+    match argv with
+    | [| path |] ->
+        let fileLines = System.IO.File.ReadAllLines path
+        let boards = readAll fileLines
+        let outLines = boards |> Seq.map (fun (n, board) -> displayField n (mapAdjacancies board)) |> List.ofSeq
+        writeFieldsTo System.Console.Out outLines
+        0
+    | _ -> 
+        printf "Usage: Minesweeper file\n"
+        -1
